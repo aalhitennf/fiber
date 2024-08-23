61e705ef8c1228385169dec75610b1af328074b3
Olli Paananen <paananen.olli@tuta.io>
2024-08-23 20:00:42 +0300

    Move app to mod

---
+++ fiber/src/app.rs
@@ -0,0 +1,154 @@
+use std::path::{Path, PathBuf};
+
+use log::LevelFilter;
+
+use crate::state::{FnPointer, State};
+
+pub struct App {
+    path: PathBuf,
+    state: State,
+    handlers: Option<Vec<(String, FnPointer)>>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        App::new()
+    }
+}
+
+impl App {
+    #[must_use]
+    pub fn new() -> Self {
+        let path = PathBuf::from("./fiber");
+        App {
+            state: State::default(),
+            path,
+            handlers: None,
+        }
+    }
+
+    #[must_use]
+    pub fn enable_logging(self) -> Self {
+        env_logger::builder()
+            .filter_module("wgpu_hal", LevelFilter::Error)
+            .filter_module("wgpu_core", LevelFilter::Error)
+            .filter_module("naga", LevelFilter::Error)
+            .filter_module("floem_cosmic_text", LevelFilter::Error)
+            .filter_level(LevelFilter::Info)
+            .init();
+
+        log::info!("Logging enabled");
+
+        self
+    }
+
+    /// # Panics
+    /// Panics if given path doesn't exists
+    #[must_use]
+    pub fn from_path(path: impl AsRef<Path>) -> Self {
+        let path = path.as_ref().join("fiber").canonicalize().expect("Invalid path");
+
+        App {
+            state: State::default(),
+            path,
+            handlers: None,
+        }
+    }
+
+    #[must_use]
+    pub fn handlers(mut self, handlers: Vec<(String, FnPointer)>) -> Self {
+        self.handlers = Some(handlers);
+        self
+    }
+
+    /// # Panics
+    /// Panics if creating Runtime fails
+    #[cfg(debug_assertions)]
+    pub fn run(mut self) {
+        use floem::ext_event::create_signal_from_channel;
+        use floem::reactive::{create_effect, provide_context, RwSignal};
+        use floem::views::{dyn_view, Decorators};
+        use floem::IntoView;
+
+        use crate::runtime::Runtime;
+        use crate::theme::{theme_provider, StyleCss, Theme, ThemeOptions};
+        use crate::{builders, StateCtx};
+
+        self.state.read_vars(&self.path.join("main.vars"));
+
+        if let Some(handlers) = self.handlers.take() {
+            for h in handlers {
+                self.state.add_handler(h);
+            }
+        }
+
+        let (sender, receiver) = crossbeam_channel::unbounded();
+
+        let runtime = RwSignal::new(Runtime::new(&self.path, sender).expect("Failed to create Runtime"));
+        let state = StateCtx::new(self.state);
+        let theme = RwSignal::new(Theme::from_path(&self.path).expect("Invalid theme path"));
+
+        let runtime_event_sig = create_signal_from_channel(receiver.clone());
+        let theme_event_sig = create_signal_from_channel(theme.get_untracked().channel.1);
+
+        provide_context(runtime);
+        provide_context(state);
+        provide_context(theme);
+
+        create_effect(move |_| {
+            if runtime_event_sig.get().is_some() {
+                runtime.update(Runtime::update_source);
+                log::info!("Sources reloaded");
+            }
+        });
+
+        create_effect(move |_| {
+            if theme_event_sig.get().is_some() {
+                theme.update(Theme::reload);
+                log::info!("Css reloaded");
+            }
+        });
+
+        let theme_provider = theme_provider(
+            move || {
+                dyn_view(move || runtime.with(|rt| builders::source(rt.source()).into_any()))
+                    .css(&["body"])
+                    .debug_name("Body")
+            },
+            ThemeOptions::with_path(self.path.join("styles")),
+        );
+
+        floem::launch(|| theme_provider);
+    }
+
+    /// # Panics
+    #[cfg(not(debug_assertions))]
+    pub fn run(mut self) {
+        self.state.read_vars(&self.path.join("main.vars"));
+
+        if let Some(handlers) = self.handlers.take() {
+            for h in handlers {
+                self.state.add_handler(h);
+            }
+        }
+
+        let state = StateCtx::new(self.state);
+        let theme = RwSignal::new(Theme::from_path(&self.path).expect("Invalid theme path"));
+
+        provide_context(state);
+        provide_context(theme);
+
+        let theme_provider = theme_provider(
+            move || {
+                // TODO This probably don't need to be dyn_view on release build and could be
+                // TODO scoped down to specific views/nodes
+                dyn_view(move || builders::source(&include_str!("../../examples/stateful/fiber/main.fml")))
+                    .css(&["body"])
+                    .debug_name("Body")
+            },
+            ThemeOptions::with_path(self.path.join("styles")),
+        );
+
+        floem::launch(|| theme_provider);
+    }
+}
--- fiber/src/lib.rs
+++ fiber/src/lib.rs
@@ -4,6 +4,7 @@
     clippy::cast_precision_loss
 )]
 
+mod app;
 mod builders;
 mod runtime;
 mod signal;
@@ -12,17 +13,10 @@ mod theme;
 
 use std::fmt::Debug;
 use std::future::Future;
-use std::path::{Path, PathBuf};
 use std::pin::Pin;
 
 use floem::ext_event::create_signal_from_channel;
-use floem::reactive::{create_effect, provide_context, use_context, RwSignal, Scope};
-use floem::views::{dyn_view, Decorators};
-use floem::IntoView;
-use log::LevelFilter;
-use runtime::Runtime;
-use state::{FnPointer, State};
-use theme::{theme_provider, StyleCss, Theme, ThemeOptions};
+use floem::reactive::{use_context, Scope};
 
 mod observer;
 
@@ -30,138 +24,8 @@ mod observer;
 pub use fiber_macro::{async_func, func};
 
 // Export common structs
+pub use app::App;
 pub use state::StateCtx;
-
-pub struct App {
-    path: PathBuf,
-    state: State,
-    handlers: Option<Vec<(String, FnPointer)>>,
-}
-
-impl Default for App {
-    fn default() -> Self {
-        App::new()
-    }
-}
-
-impl App {
-    #[must_use]
-    pub fn new() -> Self {
-        let path = PathBuf::from("./fiber");
-        App {
-            state: State::default(),
-            path,
-            handlers: None,
-        }
-    }
-
-    #[must_use]
-    pub fn enable_logging(self) -> Self {
-        enable_logging();
-        self
-    }
-
-    /// # Panics
-    /// Panics if given path doesn't exists
-    #[must_use]
-    pub fn from_path(path: impl AsRef<Path>) -> Self {
-        let path = path.as_ref().join("fiber").canonicalize().expect("Invalid path");
-
-        App {
-            state: State::default(),
-            path,
-            handlers: None,
-        }
-    }
-
-    #[must_use]
-    pub fn handlers(mut self, handlers: Vec<(String, FnPointer)>) -> Self {
-        self.handlers = Some(handlers);
-        self
-    }
-
-    /// # Panics
-    /// Panics if creating Runtime fails
-    #[cfg(debug_assertions)]
-    pub fn run(mut self) {
-        self.state.read_vars(&self.path.join("main.vars"));
-
-        if let Some(handlers) = self.handlers.take() {
-            for h in handlers {
-                self.state.add_handler(h);
-            }
-        }
-
-        let (sender, receiver) = crossbeam_channel::unbounded();
-
-        let runtime = RwSignal::new(Runtime::new(&self.path, sender).expect("Failed to create Runtime"));
-        let state = StateCtx::new(self.state);
-        let theme = RwSignal::new(Theme::from_path(&self.path).expect("Invalid theme path"));
-
-        let runtime_event_sig = create_signal_from_channel(receiver.clone());
-        let theme_event_sig = create_signal_from_channel(theme.get_untracked().channel.1);
-
-        provide_context(runtime);
-        provide_context(state);
-        provide_context(theme);
-
-        create_effect(move |_| {
-            if runtime_event_sig.get().is_some() {
-                runtime.update(Runtime::update_source);
-                log::info!("Sources reloaded");
-            }
-        });
-
-        create_effect(move |_| {
-            if theme_event_sig.get().is_some() {
-                theme.update(Theme::reload);
-                log::info!("Css reloaded");
-            }
-        });
-
-        let theme_provider = theme_provider(
-            move || {
-                dyn_view(move || runtime.with(|rt| builders::source(rt.source()).into_any()))
-                    .css(&["body"])
-                    .debug_name("Body")
-            },
-            ThemeOptions::with_path(self.path.join("styles")),
-        );
-
-        floem::launch(|| theme_provider);
-    }
-
-    /// # Panics
-    #[cfg(not(debug_assertions))]
-    pub fn run(mut self) {
-        self.state.read_vars(&self.path.join("main.vars"));
-
-        if let Some(handlers) = self.handlers.take() {
-            for h in handlers {
-                self.state.add_handler(h);
-            }
-        }
-
-        let state = StateCtx::new(self.state);
-        let theme = RwSignal::new(Theme::from_path(&self.path).expect("Invalid theme path"));
-
-        provide_context(state);
-        provide_context(theme);
-
-        let theme_provider = theme_provider(
-            move || {
-                // TODO This probably don't need to be dyn_view on release build and could be
-                // TODO scoped down to specific views/nodes
-                dyn_view(move || builders::source(&include_str!("../../examples/stateful/fiber/main.fml")))
-                    .css(&["body"])
-                    .debug_name("Body")
-            },
-            ThemeOptions::with_path(self.path.join("styles")),
-        );
-
-        floem::launch(|| theme_provider);
-    }
-}
 
 pub fn run_task<T>(task: AsyncTask<T>)
 where
@@ -226,15 +90,3 @@ where
 //         self.future
 //     }
 // }
-
-fn enable_logging() {
-    env_logger::builder()
-        .filter_module("wgpu_hal", LevelFilter::Error)
-        .filter_module("wgpu_core", LevelFilter::Error)
-        .filter_module("naga", LevelFilter::Error)
-        .filter_module("floem_cosmic_text", LevelFilter::Error)
-        .filter_level(LevelFilter::Info)
-        .init();
-
-    log::info!("Logging enabled");
-}
